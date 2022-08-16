de36b0a64fa5931b101467afdde7be69807088a3
Your Name <you@example.com>
2022-08-16 16:50:45 -0500

    added generatePassword function, prompt, and confirms

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 var specialCharacters = ["!", "#", "$", "%", "&", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[]", "^", "_", "`", "{", "|", "}", "~"];
-var empty = []
-
+var empty = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+prompt ("How many characters would you like the password to contain? Type a number between 8-128.")
+confirm ("Would you like to include special characters?")
+confirm ("Would you like to include numbers?")
+confirm ("Would you like to include lowercase letters?")
+confirm ("Would you like to include uppercase letters?")
+  
+return password
+}
 
 // Write password to the #password input
 function writePassword() {
