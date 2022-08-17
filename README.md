f61293c79333e64ade009e59241a5325b1119230
Your Name <you@example.com>
2022-08-17 16:59:42 -0500

    Wrote ReadMe and finished code with comments

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Password-Generator
+This was my first attempt at writing Javascript. For this assignment, I was given starter code that included complete files for HTML and CSS. 
+Deployed Application:
+
+## Goals of the Password Generator
+I needed to provide a series of prompts to determine:
+-The password length. I provided a prompt window, but was asked to keep the password between 8-128 characters.
+-Then I needed to establish what characters the person wanted. Options needed to include, uppercase and lowercase letters, numbers, and special characters. 
+-Once all the information was collected from the prompts and confirms, then the generated password would appear in the box.
+
+## What I Learned
+This was definitely a challenge. I genuinely learned so much valuable information. 
+The few highlights:
+-"concat" was very important for this assignment. I needed to take the other arrays that I created with all the different characters and combine them into one finalPassword variable. 
+-"parseInt" was a new one for me. This was used to change the input for the length of the password from a string type to a number type.
+-The last thing was the importance of the "for loop". I'm not even sure I have my brain totally wrapped around it, but plan to continue to study it. 
+
+## Screenshot
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Creating variable for the length of the password.
+// Variable for the length of the password.
 var passwordLength =10;
 
 //Creating variables for all the characters being offered.
@@ -8,49 +8,68 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 var specialCharacters = ["!", "#", "$", "%", "&", "()", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[]", "^", "_", "`", "{", "|", "}", "~"];
 var finalPassword = [];
 
-// Get references to the #generate element
+//Getting info from the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//This function provides the prompts and gathers that information.
+// Add event listener to generate button so that when I click, the prompts begin.
+generateBtn.addEventListener("click", writePassword);
+
+
+//This function provides the prompts and gathers the choices.
 function getChoices(){
+  finalPassword =[];
+  
+  //Learned that parseInt allows me to change type of input from string to number.
   passwordLength = parseInt(prompt ("How many characters would you like the password to contain? Type a number between 8-128."));
 
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-      alert("Password length has to be a number between 8-128. Try again.");
-      return false;
+    //I wanted an alert in the event that the right info was given.
+    alert("Password length has to be between 8-128 characters. Try again.");
+    return false;
   }
 
+  //Used concat syntax to add all the different arrays as prompted to my empty finalPassword array.
  if(confirm ("Would you like to include lowercase letters?")) {
-    finalPassword.concat(lowerCase);
+    finalPassword = finalPassword.concat(lowerCase);
 
   }
   
   if(confirm ("Would you like to include uppercase letters?")){
-    finalPassword.concat(upperCase);
+    finalPassword = finalPassword.concat(upperCase);
   }
   
   if(confirm ("Would you like to include numbers?")) {
-    finalPassword.concat(numbers);
+    finalPassword = finalPassword.concat(numbers);
   }
   
   if(confirm ("Would you like to include special characters?")) {
-    finalPassword.concat(specialCharacters);
+    finalPassword = finalPassword.concat(specialCharacters);
   }
+  return true
 }
-return true
 
+//This function now takes the information collected and generates the random password. Used for loop.
 function generatePassword(){
-
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * finalPassword.length);
+    password = password + finalPassword[randomIndex];
+  }
+  return password;
 }
 
-// Write password to the #password input
+// This function then displays the generated password as it was added in event listener.
 function writePassword() {
-  var password = getChoices()//generatePassword();
+  var correctPrompts = getChoices();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    
+ if(correctPrompts) {
+    var newPassword = generatePassword();
+    
+    passwordText.value = newPassword;
+  
+  }    
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
